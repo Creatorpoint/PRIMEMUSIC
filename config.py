77f454c104dddc95a1c6e8b811b876d8cd3e496d
Creatorpoint <pk4030794@gmail.com>
2026-02-01 10:51:52 +0530

    Update environment variables and image URLs

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID", None))
-OWNER_USERNAME = os.getenv("OWNER_USERNAME", "WTF_WhyMeeh")
-BOT_USERNAME = os.getenv("BOT_USERNAME", "ShrutixMusicBot")
+OWNER_USERNAME = os.getenv("OWNER_USERNAME", "PREMGUPTA3M")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "Prime_music07_bot")
 
 MONGO_DB_URI = os.getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", None))
@@ -21,11 +21,11 @@ UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/NoxxOP/ShrutiMusi
 UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = os.getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/ShrutiBots")
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/ShrutiBotSupport")
-INSTAGRAM = os.getenv("INSTAGRAM", "https://instagram.com/yaduwanshi_nand")
-YOUTUBE = os.getenv("YOUTUBE", "https://youtube.com/@NandEditz")
-GITHUB = os.getenv("GITHUB", "https://github.com/NoxxOP")
+SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/PRIME_MUSIC08")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/PRIME_X_CHAT2")
+INSTAGRAM = os.getenv("INSTAGRAM", "https://www.instagram.com/prem_gupta_2m?igsh=MWFiNGx5NTl6Y3Npdw==")
+YOUTUBE = os.getenv("YOUTUBE", "https://t.me/PRIME_MUSIC08")
+GITHUB = os.getenv("GITHUB", "https://github.com/Creatorpoint/PRIMEMUSIC.git")
 DONATE = os.getenv("DONATE", "https://t.me/ShrutiBots/91")
 PRIVACY_LINK = os.getenv("PRIVACY_LINK", "https://graph.org/Privacy-Policy-05-01-30")
 
@@ -46,7 +46,7 @@ STRING5 = os.getenv("STRING_SESSION5", None)
 
 AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
 
-START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/7q8bfg.jpg")
+START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/y609qm.jpeg")
 PING_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
 PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"

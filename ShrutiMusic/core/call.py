aad34a56361a80b2f0921d8e8c351a50c815b57f
Creatorpoint <pk4030794@gmail.com>
2026-02-08 20:31:04 +0530

    Update call.py

--- ShrutiMusic/core/call.py
+++ ShrutiMusic/core/call.py
@@ -28,10 +28,7 @@ from typing import Union
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
 from pytgcalls import PyTgCalls
-from pytgcalls.exceptions import (
-    NoActiveGroupCall,
-    TelegramServerError,
-)
+from pytgcalls.exceptions import NoActiveGroupCall
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
 from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo

6f1684ac7c106319dbbdf46fce3942710c822359
Creatorpoint <pk4030794@gmail.com>
2026-03-22 06:54:40 +0530

    Update start.py

--- ShrutiMusic/plugins/bot/start.py
+++ ShrutiMusic/plugins/bot/start.py
@@ -71,8 +71,8 @@ async def start_pm(client, message: Message, _):
 
             # ✅ Remove BlushMusicbot redirect link
             help_text = help_text.replace(
-                "https://t.me/PRIME_X_CHAT2",
-                f"https://t.me/Prime_music07_bot?start=help"
+                "https://t.me/Prime_music07_bot?start=help",
+                f"https://t.me/{BOT_USERNAME}?start=help"
             )
 
             return await message.reply_text(
